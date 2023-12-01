1a7f8126db77bcb928c03c8156811c9ae7b720ac
AlexanderRibeiro <137852897+AlexanderRibeiro@users.noreply.github.com>
2023-11-30 21:11:16 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('feedbackForm').addEventListener('submit', function (event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    // You can perform additional validation here if needed
+
+    // Display thank you message using an alert
+    alert('Obrigado pelo seu feedback!');
+});
